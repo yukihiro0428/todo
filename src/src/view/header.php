bd2dd030a3aa9bf7bd3e7f3a2a3fda1c31e94580
yukihiro kawakami <do_everything_in@icloud.com>
2020-10-21 10:25:34 +0900

    css?ver=**変更

--- src/src/view/header.php
+++ src/src/view/header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>TODOリスト</title>
-	<link type="text/css" rel="stylesheet" href="css/style.css?ver=021118" />
+	<link type="text/css" rel="stylesheet" href="css/style.css?ver=20201021" />
 </head>
 
 <body>
