43e4a60647d5c772c834e714952e75e8b117ddc8
yukihiro kawakami <do_everything_in@icloud.com>
2020-10-22 13:36:48 +0900

    front-page修正

--- src/src/view/header.php
+++ src/src/view/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>TODOリスト</title>
+	<title>TODOアプリ</title>
 	<link type="text/css" rel="stylesheet" href="css/style.css?ver=2020102101" />
 </head>
 
